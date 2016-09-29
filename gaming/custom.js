072b03becf83abc235b1689c6bfc937f9cc70cc4
queenming <xleahmarie@gmail.com>
2016-09-29 01:02:18 -0400

    large .load() overhaul

--- gaming/custom.js
+++ gaming/custom.js
@@ -7,7 +7,26 @@ $(document).ready(function(){
 	});
 
 	
-
      $('table tr td:nth-child(3)').addClass("hidden-xs");
+	 
+	 $("#playing").click(function(event){
+               $('#stage').load('playing.html');
+            });
+	 
+	 $("#toplay").click(function(event){
+               $('#stage').load('toplay.html');
+            });
+			
+	 $("#played").click(function(event){
+               $('#stage').load('played.html');
+            });
+			
+	 $("#ids").click(function(event){
+               $('#stage').load('idpage.html');
+            });
+			
+	$("#review").click(function(event){
+               $('#stage').load('/reviews/smtivapocalypse.html');
+            });
 
 });
